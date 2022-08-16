2090698caf79f55736b6fe9330186d9061f3677b
Étor Lucca <etor.lucca78@gmail.com>
2022-08-16 12:10:38 -0300

    commit para entrega e deploy do projeto

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+//------ Caso queira rodar o servidor no localhost inverter as baseURL.
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://task-calendar.glitch.me"
 });
 
 export const createSession = async (email, password) => {
